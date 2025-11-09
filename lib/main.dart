ba513946c84272c05f9c20e09801c685a375d1a3
ZyVoir <whcs.william@gmail.com>
2025-11-09 20:41:59 +0700

    chore(supabase): .env + supabase initialization
    env added to git ignore, will be given upon request

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,22 @@ import 'package:stocklyzer/config/language.dart';
 import 'package:stocklyzer/controller/onboardingController.dart';
 import 'package:stocklyzer/controller/themeController.dart';
 import 'package:stocklyzer/view/onboarding.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  if (supabaseUrl == null || supabaseAnonKey == null) {
+    throw Exception('Supabase URL or Anon Key is not set in .env file');
+  }
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+
   Get.put(Themecontroller());
   Get.put(Onboardingcontroller());
   runApp(const MyApp());
